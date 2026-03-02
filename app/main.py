89cc69e988c91860650dff2db8f96e1725722249
Reverad <sektumpro@gmail.com>
2026-03-02 19:29:49 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -22,10 +22,10 @@ class Distance:
         self.km += self.instance(other)
         return self
 
-    def __mul__(self, other: float) -> Distance:
+    def __mul__(self, other: int | float) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: float) -> Distance:
+    def __truediv__(self, other: int | float) -> Distance:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: DistAnnot) -> bool:
